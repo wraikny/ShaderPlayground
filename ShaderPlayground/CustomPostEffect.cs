b6419f93545a3385bc917cf91025b0836313216c
wraikny <writtenknown@gmail.com>
2019-01-23 20:13:54 +0900

    post effect file load

--- ShaderPlayground/CustomPostEffect.cs
+++ ShaderPlayground/CustomPostEffect.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
+
 namespace ShaderPlayground
 {
     public class CustomPostEffect : asd.PostEffect
     {
-        private readonly string filename;
+        private string filename;
 
         private asd.Shader2D shader;
         private asd.Material2D material2d;
@@ -12,20 +14,38 @@ namespace ShaderPlayground
 
         public CustomPostEffect(string filename)
         {
-            filename = filename;
+            this.filename = filename;
+            SetMaterial();
         }
 
         private void SetMaterial()
         {
-            // シェーダーをHLSL/GLSLから生成する。
-            shader = asd.Engine.Graphics.CreateShader2D(filename);
-
-            // シェーダーからマテリアルを生成する。
-            material2d = asd.Engine.Graphics.CreateMaterial2D(shader);
+            if(asd.Engine.File.Exists(filename))
+            {
+                {
+                    var buf = asd.Engine.File.CreateStaticFile(filename).Buffer;
+                    var s = System.Text.Encoding.UTF8.GetString(buf);
+                    
+                    // シェーダーをHLSL/GLSLから生成する。
+                    shader = asd.Engine.Graphics.CreateShader2D(s);
+                    
+                    // シェーダーからマテリアルを生成する。
+                    material2d = asd.Engine.Graphics.CreateMaterial2D(shader);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"File '{filename}' does not exist");
+            }
         }
 
         protected override void OnDraw(asd.RenderTexture2D dst, asd.RenderTexture2D src)
         {
+            if(asd.Engine.Keyboard.GetKeyState(asd.Keys.Space) == asd.KeyState.Release)
+            {
+                SetMaterial();
+            }
+
             time = time + 0.05f;
             material2d.SetFloat("time", time);
             material2d.SetVector2DF("windowSize", asd.Engine.WindowSize.To2DF());
