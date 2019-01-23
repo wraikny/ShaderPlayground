f93e1950ea5c757641fbbc70cd5191ad593f7128
wraikny <writtenknown@gmail.com>
2019-01-23 20:13:41 +0900

    altseed program

--- ShaderPlayground/Program.cs
+++ ShaderPlayground/Program.cs
@@ -6,7 +6,26 @@ namespace ShaderPlayground
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Altseedを初期化する。
+            asd.Engine.Initialize("ShaderPlayground", 800, 450, new asd.EngineOption());
+
+            // シーン、レイヤー、画像を表示するオブジェクトを生成する。
+            var scene = new asd.Scene();
+            var layer = new asd.Layer2D();
+
+            // シーンを変更し、そのシーンにレイヤーを追加し、そのレイヤーにオブジェクトを追加する。
+            asd.Engine.ChangeScene(scene);
+            scene.AddLayer(layer);
+
+            // レイヤーにポストエフェクトを適用する。
+            layer.AddPostEffect(new CustomPostEffect("ShaderCode/01/sample.glsl"));
+
+            while (asd.Engine.DoEvents())
+            {
+                asd.Engine.Update();
+            }
+
+            asd.Engine.Terminate();
         }
     }
 }
